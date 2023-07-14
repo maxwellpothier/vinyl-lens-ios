f706b0725f3a4acc2c7377500d9e024437774b98
maxwellpothier <pothiermaxwell@gmail.com>
2023-07-13 23:50:51 -0600

    Initial Commit

---
+++ AI Album Info/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AI Album Info
+//
+//  Created by Max Pothier on 7/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
